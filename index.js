109a421f96a60ee0d636991c570e9e72673c195e
Cheryl Harrison <107930821+Che-ryl@users.noreply.github.com>
2026-09-23 15:26:56 +0400

    Change background image URL to full size

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 try{
     const res = await fetch("https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=nature")
     const data = await res.json()
-    document.body.style.backgroundImage = `url(${data.urls.regular})`
+    document.body.style.backgroundImage = `url(${data.urls.full})`
     document.getElementById("author").textContent = `By: ${data.user.name}`
 }
 catch(err){
