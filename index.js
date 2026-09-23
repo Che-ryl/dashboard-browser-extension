a2777cf20a924d45dc522be903f452de4cba270c
Cheryl Harrison <107930821+Che-ryl@users.noreply.github.com>
2026-09-23 15:28:51 +0400

    Change background image URL to use 'raw' version

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 try{
     const res = await fetch("https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=nature")
     const data = await res.json()
-    document.body.style.backgroundImage = `url(${data.urls.full})`
+    document.body.style.backgroundImage = `url(${data.urls.raw})`
     document.getElementById("author").textContent = `By: ${data.user.name}`
 }
 catch(err){
